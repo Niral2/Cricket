8ed060c561c2fb6b024976fee0a44cdb977de790
Niral2 <g.niral@yahoo.ca>
2017-02-09 18:00:30 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>MS Dhoni</title>
+	<link rel="shortcut icon" href="images/ms.png">
+	<link rel="stylesheet" href="styles/normalize.css">
+	<link rel="stylesheet" href="styles/main.css">
+	<link rel="stylesheet" href="styles/index.css">
+</head>
+
+<body>
+	<header>
+		<img src="images/logo.png" alt="Town Hall logo" height="67">
+		<img src="images/header_thumb.png" alt="Mahendra Picture" border="0" height="112">
+
+
+	</header>
+	<nav id="nav_menu">
+    	<ul>
+        	<li><a href="index.html" class="current">Home</a></li>
+			<li><a href="index.html">Cricket</a>
+			<ul>
+                	<li><a href="index.html" class="current">BATTING</a>
+					<ul>
+					<li><a href="cricket.html" class="current">STATISTICS</a></li>
+					</ul>
+					<li><a href="index.html" class="current">BOWLING</a>
+					<ul>
+					<li><a href="cricket.html" class="current">STATISTICS</a></li>
+					</ul>
+
+					</li>
+			</ul>
+        	<li><a href="photo.html">Photo Gallery</a></li>
+
+        	<li><a href="feedback.html" class="current">FEEDBACK</a></li>
+    	</ul>
+	</nav>
+	<main>
+		<section>
+
+			<h1>BIOGRAPHY</h1>
+			<article>
+
+				<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Dhoni has done his schooling from DAV Jawahar Vidya Mandir, Shyamali (now the school is known as JVM, Shyamli, Ranchi).
+				Before he stepped into cricketing career, he excelled in badminton and football and got selected at district as well club level in these two sports.
+				MS Dhoni played as a goalkeeper for his football team in those days and was sent to play cricket for a cricket club
+				that was local by his football coach.</p><br>
+
+				<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;He made his cricketingdebut ina cricket team of Bihar in Jharkhand during the 1998-1999 cricketseasons as an eighteen year old.Subsequently,
+				he was chosen to represent India-A for a cricket tour to Kenya in 2004.Apart from being one of the most successful captains of the Indian cricket team
+				till now;he is a wicketkeeper, right hand batsman as well as right hand medium paced bowler. </p>
+
+			</article>
+
+
+		</section>
+
+		<aside id="asideLeft">
+                <h2>Navigation</h2>
+                <a href="index.html">
+                    <br><img src="images/Home.png" alt="Home" height="80px"/></a>
+                <a href="cricket.html">
+                    <br><img src="images/cricket.png" alt="Cricket" height="80px"/></a>
+                <a href="photo.html">
+                    <br><img src="images/photo.png" alt="Level" height="80px"></a>
+                <a href="feedback.html">
+                    <br><img src="images/Feedback.png" alt="Feedback" height="80px"></a>
+            </aside>
+
+		<aside id="right-side">
+
+			<div id="news_left">
+	<div id="left_heading">
+		<div id="head_pic"><img src="images/latest_news.png" alt="" border="0"></div>
+	</div>
+		<marquee direction="up" scrollamount="3" onmouseover="this.stop();" onmouseout="this.start();">
+		<ul id="ticker">
+
+		    <li>
+				<span class="news_title">Mahendra Singh Dhoni</span>
+				<span style="color: rgb(0, 157, 224);">05/23/2012</span>
+				<p>&nbsp;Mahendra Singh Dhoni has emerged as one of India's most successful captain this cricket...<br>
+				<a href="news.php?news_id=17"></a></p><hr>
+			</li><li>
+				<span class="news_title">MS DHONI</span>
+				<span style="color: rgb(0, 157, 224);">05/17/2012</span>
+				<p>Dhoni made his mark in the international cricket soon after his ODI debut in December...<br>
+				<a href="news.php?news_id=21"></a></p><hr>
+			</li><li>
+				<span class="news_title">Indian Captain MS Dhoni</span>
+				<span style="color: rgb(0, 157, 224);">05/16/2012</span>
+				<p>&nbsp;Indian Captain MS Dhoni gifted Akshay Kumar the gloves he wore during the match against...<br>
+				<a href="news.php?news_id=16"></a></p><hr>
+			</li><li>
+				<span class="news_title">MS Dhoni and Coach Duncan Fletcher</span>
+				<span style="color: rgb(0, 157, 224);">05/07/2012</span>
+				<p>Mahendra Singh Dhoni has emerged as one of India's most successful captain this cricket crazy...<br>
+				<a href="news.php?news_id=15"></a></p><hr>
+			</li><li>
+				<span class="news_title">Fastest hundred scored by an Indian wicket-keeper.</span>
+				<span style="color: rgb(0, 157, 224);">05/31/2012</span>
+				<p>&nbsp;MS Dhoni's maiden ton came off 93 balls and is the fastest hundred scored by an Indian...<br>
+				<a href="news.php?news_id=20"></a></p><hr>
+			</li><li>
+				<span class="news_title">Mahendra Singh Dhoni ODI</span>
+				<span style="color: rgb(0, 157, 224);">05/30/2012</span>
+				<p>&nbsp;Dhoni made his mark in the international cricket soon after his ODI debut in December 2004...<br>
+				<a href="news.php?news_id=19"></a></p><hr>
+			</li><li>
+				<span class="news_title">Mahendra Singh Dhoni Cool Captain</span>
+				<span style="color: rgb(0, 157, 224);">05/25/2012</span>
+				<p>&nbsp;The captain cool has led the team to many first under his captaincy.The captain cool has...<br>
+				<a href="news.php?news_id=18"></a></p><hr>
+			</li></ul>
+  </marquee>
+</div>
+
+		</aside>
+	</main>
+	<footer>
+		<p>&copy; 2016, Niral Gandhi</p>
+	</footer>
+</body>
+</html>
